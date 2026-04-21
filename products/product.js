795321bb96b1ec8d5f937484d9b0d4c05982e5c8
JULIA <jsorgen13@gmail.com>
2026-04-21 12:07:04 -0400

    More images and adding them to site

--- products/product.js
+++ products/product.js
@@ -29,7 +29,7 @@ const PRODUCTS = [
       "images/castlerock/CastleRockPorch.png",
       "images/castlerock/CastleRockWall.png"
     ],
-    description: "This stone offers a rich, warm, natural look, with shades of brown, tan, rust and grey which is perfect for rustic, country manor and casual settings.",
+    description: "This stone offers a rich, warm, natural look, with shades of brown, tan, rust and gray which is perfect for rustic, country manor and casual settings.",
     specs: {
       "Stone Type": "Square & Rectangle",
       "Colors": "Blended browns, tans, rusts and grays"
@@ -87,7 +87,7 @@ const PRODUCTS = [
       "images/hydepark/HydeParkRR3.webp",
       "images/hydepark/HydeParkSR.jpg"
     ],
-    description: "This stone offers a rich, natural, weathered look in toness of brown, tan, green and grey, which is perfect for rustic, country manor, contemporary and casual settings.  ",
+    description: "This stone offers a rich, natural, weathered look in toness of brown, tan, green and gray, which is perfect for rustic, country manor, contemporary and casual settings.  ",
     specs: {
       "Stone Type": "Ledgestone, Square & Rectangle, Mosaic, Mini Ledgestone",
       "Colors": "Blended browns, tans, greens and grays"
@@ -96,8 +96,278 @@ const PRODUCTS = [
   {
     id: "pinehurst",
     name: "Pinehurst",
-    categories: [""]
-  }
+    categories: ["ledgestone","square-rectangle","mosaic"],
+    cover: "images/pinehurst/PinehurstPanel.jpg",
+    images:[
+      "images/pinehurst/PinehurstFront.webp",
+      "images/pinehurst/PinehurstInstall.jpg",
+      "images/pinehurst/PinehurstLedge.webp",
+      "images/pinehurst/PinehurstLedge2.webp",
+      "images/pinehurst/PinehurstLedge3.webp",
+      "images/pinehurst/PinehurstSR.jpg"
+    ],
+    description: "",
+    specs: {
+      "Stone Type": "Ledgestone, Square & Rectangle, Mosaic",
+      "Colors": "Shades of buff, yellow, gray and green"
+    }
+  },
+  {
+    id: "brindle",
+    name: "Brindle",
+    categories: [""],
+    cover: "images/brindle/BrindlePanelMortar.jpg",
+    images:[
+      "images/brindle/BrindlePanelMortar.jpg",
+      "images/brindle/BrindlePanelDry.jpg",
+      "images/brindle/Brindle.jpg"
+    ],
+    description: "",
+    specs: {
+      "Stone Type": "",
+      "Colors": ""
+    }
+
+  },
+  {
+    id: "countrywine",
+    name: "Country Wine",
+    categories: ["ledgestone"],
+    cover: "images/countrywine/CountryWinePanel.jpg",
+    images:[
+      "images/countrywine/CountryWineDoor.jpg",
+      "images/countrywine/CountryWineHouse.jpg",
+      "images/countrywine/CountryWineWall.jpg"
+    ],
+    description: "Country Wine is a limestone that offers exquisite coloration with its weathered, split face.",
+    specs: {
+      "Stone Type": "Ledgestone",
+      "Colors": "Soft tones of Gray, Peach, Lavender, Yellow"
+    }
+  },
+  {
+    id: "poconobrown",
+    name: "Pocono Brown",
+    categories: ["ledgestone"],
+    cover: "images/poconobrown/PoconoBrownLoose.jpg",
+    images:[
+      "images/poconobrown/PoconoBrown1.jpg",
+      "images/poconobrown/PoconoBrown2.jpg",
+      
+      "images/poconobrown/PoconoBrown4.jpg",
+      "images/poconobrown/PoconoBrownFireplace.JPG",
+      "images/poconobrown/PoconoBrownFireplace2.JPG",
+      "images/poconobrown/PoconoBrownFireplace3.JPG",
+      "images/poconobrown/PoconoBrownFireplace4.JPG",
+      "images/poconobrown/PoconoBrownFireplace5.JPG",
+      "images/poconobrown/PoconoBrownHouse.JPG",
+      "images/poconobrown/PoconoBrownHouse2.JPG",
+      "images/poconobrown/PoconoBrownLedge.jpg",
+      "images/poconobrown/PoconoBrownWall.JPG",
+      "images/poconobrown/PoconoBrownWall2.jpg"
+    ],
+    description: "Pocono Brown offers a range of colors from rich browns to neutral creams and tans, with a blend of natural weathered face and split face on the exposed face surface.  This stone offers a rich, warm, natural look, which is perfect for rustic, country manor and casual settings.",
+    specs: {
+      "Stone Type": "Ledgestone",
+      "Colors": "Antique tones – rich browns, creams, black"
+    }
+  },
+  {
+    id: "ravenwood",
+    name: "Ravenwood",
+    categories: ["square-rectangle","mosaic"],
+    cover: "images/ravenwood/RavenwoodSR.jpg",
+    images:[ 
+      "images/ravenwood/RavenwoodChimney.jpg",
+      "images/ravenwood/RavenwoodChimney2.jpg",
+      "images/ravenwood/RavenwoodFS1.jpg",
+      "images/ravenwood/RavenwoodFS3.jpg",
+      "images/ravenwood/RavenwoodFS4.jpg",
+      "images/ravenwood/RavenwoodFS5.jpg",
+      "images/ravenwood/RavenwoodMosaic.jpg",
+      "images/ravenwood/RavenwoodSq1.jpg",
+      "images/ravenwood/RavenwoodSq2.jpg",
+      "images/ravenwood/RavenwoodSquare.jpg",
+      "images/ravenwood/RavenwoodSR.jpg",
+      "images/ravenwood/RavenwoodWindows.jpg"
+    ],
+    description: "Ravenwood Blend is a sandstone found in North Central Pennsylvania.",
+    specs: {
+      "Stone Type": "Square & Rectangle, Mosaic",
+      "Colors": "Browns, grays, tans, and plums"
+    }
+  },
+  {
+    id: "shimmeringdiamonds",
+    name: "Shimmering Diamonds",
+    categories: ["square-rectangle"],
+    cover: "images/shimmeringdiamonds/SDBlock.jpg",
+    images: [
+      "images/shimmeringdiamonds/SDHouse1.jpg",
+      "images/shimmeringdiamonds/SDHouse2.jpg",
+      "images/shimmeringdiamonds/SDHouse3.jpg"
+    ],
+    description: "",
+    specs: {
+      "Stone Type": "Square & Rectangle",
+      "Colors": "Light Gray with small amounts of Yellowish Gray"
+    }
+  },
+  {
+    id: "bluestone",
+    name: "Bluestone",
+    categories: [""],
+    cover: "images/bluestone/BluestoneBlend.jpg",
+    images:[
+      "images/bluestone/BluestoneBlend.jpg",
+      "images/bluestone/BluestoneLedge.jpg",
+      "images/bluestone/BluestoneLedge2.jpg",
+    ],
+    description: "",
+    specs: {
+      "Stone Type": "",
+      "Colors": ""
+    }
+  },
+  {
+    id: "celedon",
+    name: "Celedon",
+    categories: [""],
+    cover: "images/celedon/CeledonPanel.jpg",
+    images:[
+      "images/celedon/CeledonPanel.jpg",
+      "images/celedon/CeledonStack.jpg"
+    ],
+    description: "",
+    specs: {
+      "Stone Type": "",
+      "Colors": ""
+    }
+  },
+  {
+    id: "crystaldark",
+    name: "Crystal Dark",
+    categories: [""],
+    cover: "images/crystaldark/CrystalDarkPanel.jpg",
+    images:[
+      "images/crystaldark/CrystalDarkPanel.jpg",
+    ],
+    description: "",
+    specs: {
+      "Stone Type": "",
+      "Colors": ""
+    }
+  },
+  {
+    id: "crystallite",
+    name: "Crystal Lite",
+    categories: [""],
+    cover: "images/crystallite/CrystalLitePanel.jpg",
+    images:[
+      "images/crystallite/CrystalLitePanel.jpg",
+      "images/crystallite/CrystalLite1.jpg",
+    ],
+    description: "",
+    specs: {
+      "Stone Type": "",
+      "Colors": ""
+    }
+  },
+  {
+    id: "desertsunburst",
+    name: "Desert Sunburst",
+    categories: [""],
+    cover: "images/desertsunburst/DesertSunburstPanel.jpg",
+    images:[
+      "images/desertsunburst/DesertSunburstPanel.jpg",
+      "images/desertsunburst/DesertSunburst1.jpg",
+      "images/desertsunburst/DesertSunburst2.jpg",
+    ],
+    description: "",
+    specs: {
+      "Stone Type": "",
+      "Colors": ""
+    }
+  },
+  {
+    id: "earthblend",
+    name: "Earth Blend",
+    categories: ["mosaic"],
+    cover: "images/earthblend/EarthBlendMosaic.jpg",
+    images:[
+      "images/earthblend/EarthBlendMosaic.jpg",
+    ],
+    description: "",
+    specs: {
+      "Stone Type": "",
+      "Colors": ""
+    }
+  },
+  {
+    id: "nittany",
+    name: "Nittany",
+    categories: [""],
+    cover: "images/nittany/NittanyPanel.jpg",
+    images:[
+      "images/nittany/NittanyBuff.jpg",
+      "images/nittany/NittanyFireplace.jpg",
+      "images/nittany/NittanyLedgeInstall.jpg"
+    ],
+    description: "",
+    specs: {
+      "Stone Type": "",
+      "Colors": ""
+    }
+  },
+  {
+    id: "silverbay",
+    name: "Silver Bay",
+    categories: [""],
+    cover: "images/silverbay/SilverBayPanel.jpg",
+    images:[
+      "images/silverbay/SilverBay1.jpg",
+      "images/silverbay/SilverBay2.jpg",
+      "images/silverbay/SilverBay3.jpg",
+      "images/silverbay/SilverBayFireplace.jpg"
+    ],
+    description: "",
+    specs: {
+      "Stone Type": "",
+      "Colors": ""
+    }
+  },
+  {
+    id: "timberridge",
+    name: "Timber Ridge",
+    categories: ["mosaic","ledgestone"],
+    cover: "images/timberridge/TimberRidgePanel.webp",
+    images:[
+      "images/timberridge/TimberRidgePanel.webp",
+      "images/timberridge/TimberRidgeMosaic.jpg",
+      "images/timberridge/TimberRidgeKitchen.jpg",
+      "images/timberridge/TimberRidgeWall.png"
+    ],
+    description: "",
+    specs: {
+      "Stone Type": "Mosaic, Ledgestone",
+      "Colors": ""
+    }
+  },
+  {
+    id: "wildginger",
+    name: "Wild Ginger",
+    categories: [""],
+    cover: "images/wildginger/WildGinger.jpg",
+    images:[
+      "images/wildginger/WildGinger.jpg",
+    ],
+    description: "",
+    specs: {
+      "Stone Type": "",
+      "Colors": ""
+    }
+  },
+
 ];
 
 const gridEl = document.getElementById("productsGrid");
